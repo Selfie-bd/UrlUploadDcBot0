b87a0152f22140312a5c4d6d3ebd59fe5af4b872
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-07 15:11:25 +0530

    Update cd_buttons.py

--- plugins/cd_buttons.py
+++ plugins/cd_buttons.py
@@ -140,6 +140,8 @@ async def button(bot, update):
 @Client.on_callback_query()
 async def formatbuttons(bot, update):
        
-    if "|" in update.data:
+    if "*" in update.data:
         await zee5_execute(bot, update)
         
+    elif "closeformat" in update.data:     
+        await update.message.delete()
