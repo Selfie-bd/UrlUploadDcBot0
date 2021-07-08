5aa258449a25978a116072cdf6bbd66dcc87e718
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-08 14:18:34 +0530

    Update cd_buttons.py

--- plugins/cd_buttons.py
+++ plugins/cd_buttons.py
@@ -122,8 +122,6 @@ async def button(bot, update):
             )
     if "|" in update.data:
         await youtube_dl_call_back(bot, update)
-    elif "*" in update.data:
-        await zee5_execute(bot, update)
     elif "=" in update.data:
         await ddl_call_back(bot, update)
     elif update.data == "home":
