ba001881befd89de9deb18f74460d07d4bb054b2
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-10 14:49:43 +0530

    Update cd_buttons.py

--- plugins/cd_buttons.py
+++ plugins/cd_buttons.py
@@ -9,19 +9,6 @@ import os
 import shutil
 import subprocess
 import time
-import pyrogram
-
-from plugins.zee5_dl import zee5_execute
-
-
-@pyrogram.Client.on_callback_query()
-async def formatbuttons(bot, update):
-       
-    if "|" in update.data:
-        await zee5_execute(bot, update)
-        
-    elif "closeformat" in update.data:     
-        await update.message.delete()
 
 
 if bool(os.environ.get("WEBHOOK", False)):
