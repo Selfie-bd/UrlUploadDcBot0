b3d9d96f45ede7a0e13bc82be57e8aac021b1c97
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-11-08 04:21:40 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -9,6 +9,6 @@ from plugins.broadcast import broadcast_handler
 
 
 
-@Client.on_message(filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply & filters.edited)
+@Client.on_message(filters.command(["broadcast"]) & filters.user(Config.OWNER_ID) & filters.reply & filters.edited)
 async def broadcast_in(_, m: Message):
     await broadcast_handler(m)
