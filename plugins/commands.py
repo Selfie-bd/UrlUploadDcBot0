42654121b046383f77e950093db9d12101abfa09
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-11-08 01:24:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,7 +39,8 @@ async def get_me_info(bot, update):
         disable_web_page_preview=True,
         reply_markup=Translation.ABOUT_BUTTONS
     )
+ 
 @Client.on_message(filters.private & filters.command("broadcast") & filters.user(Config.BOT_OWNER) & filters.reply)
-async def _broadcast(_, event: Message):
+async def broadcast(_, event: Message):
     await broadcast_handler(event)
     
