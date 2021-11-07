75a080aae98991d611fb2bddea2eeca3b4314971
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-11-08 03:49:00 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,5 +1,3 @@
-import shutil
-import psutil
 from pyrogram import filters
 from pyrogram.types import (
     Message
@@ -7,30 +5,8 @@ from pyrogram.types import (
 from sample_config import Config
 from helper_funcs.client import Client
 from helper_funcs.database import db
-from helper_funcs.display_progress import humanbytes
 from plugin.broadcast import broadcast_handler
 
-
-@Client.on_message(filters.command("status") & filters.user(Config.OWNER_ID) & ~filters.edited)
-async def status_handler(_, m: Message):
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
-    await m.reply_text(
-        text=f"**Total Disk Space:** {total} \n"
-             f"**Used Space:** {used}({disk_usage}%) \n"
-             f"**Free Space:** {free} \n"
-             f"**CPU Usage:** {cpu_usage}% \n"
-             f"**RAM Usage:** {ram_usage}%\n\n"
-             f"**Total Users in DB:** `{total_users}`",
-        parse_mode="Markdown",
-        quote=True
-    )
 
 
 @Client.on_message(filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply & ~filters.edited)
