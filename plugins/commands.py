90fc8904d920c22f30354f81705eae140da12352
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-11-08 04:26:44 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,44 @@
+import os
+import os
+import time
+import psutil
+import shutil
+import string
+import asyncio
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+from translation import Translation
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from plugins.broadcast import broadcast_handler
+
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
+
+@Client.on_message(filters.command(["help"]) & filters.private)
+async def help_user(bot, update):
+    await update.reply_text(
+        text=Translation.HELP_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=Translation.HELP_BUTTONS
+    )
+
+@Client.on_message(filters.command(["about"]) & filters.private)
+async def get_me_info(bot, update):
+    await update.reply_text(
+        text=Translation.ABOUT_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=Translation.ABOUT_BUTTONS
+    )
+
+@Client.on_message(filters.command(["broadcast"]) & filters.private & filters.user(Config.BOT_OWNER) & filters.reply)
+async def _broadcast(_, event: Message):
+    await broadcast_handler(event)
