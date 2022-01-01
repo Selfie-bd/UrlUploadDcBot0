00c4a9e6ad89d02f3b95fb5d24c10c70e6a900d8
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-02 02:27:14 +0530

    Delete plugins/broadcast directory

--- plugins/broadcast/new/__init__.py
+++
@@ -1,6 +0,0 @@
-from plugins.broadcast.new.custom_uploader import CustomUploader
-from plugins.broadcast.new.send_flooded_message import SendFloodedMessage
-
-
-class New(CustomUploader, SendFloodedMessage):
-    """ New Methods for Pyrogram """
