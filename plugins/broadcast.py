b94576d3dca2535b761dbe10bf6bfe7d28619f67
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-02 00:37:03 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -8,7 +8,7 @@ import datetime
 import aiofiles.os
 import traceback
 from sample_config import Config
-from plugins.dbs.access_db import db
+from plugins.dbs.database import db
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
