c9a16a8c8968d7133a702eb34c21a5cf9a995aa4
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-02 02:47:15 +0530

    Update access.py

--- plugins/database/access.py
+++ plugins/database/access.py
@@ -1,4 +1,4 @@
 from sample_config import Config
 from plugins.database.database import Database
 
-clinton = Database(Config.DB_URI, Config.SESSION_NAME)
+clinton = Database(Config.MONGODB_URI, Config.SESSION_NAME)
