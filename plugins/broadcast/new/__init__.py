d0c0514bf6fd55130d4fc4ed9c60feaba04b011a
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-02 01:39:22 +0530

    Update __init__.py

--- plugins/broadcast/new/__init__.py
+++ plugins/broadcast/new/__init__.py
@@ -1,6 +1,5 @@
-# (c) @AbirHasan2005
-
-from .send_flooded_message import SendFloodedMessage
+from pligins.broadcast.new.custom_uploader import CustomUploader
+from pligins.broadcast.new.send_flooded_message import SendFloodedMessage
 
 
 class New(CustomUploader, SendFloodedMessage):
