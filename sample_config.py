aa73821d738f9ec83723ba4df242c0d8040d264b
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-02 01:44:03 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,12 @@
 import os
+import logging
+
+logging.basicConfig(
+    format='%(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO
+)
 
 class Config(object):
 
