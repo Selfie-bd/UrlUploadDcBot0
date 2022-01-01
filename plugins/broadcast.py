92efcbb5671fd4aa0990f819b0be48781b58fb79
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-01 23:07:26 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -38,7 +38,7 @@ async def send_msg(user_id, message):
         return 500, f"{user_id} : {traceback.format_exc()}\n"
 
 
-async def broadcast_(m: Message):
+async def broadcast_handler(m: Message):
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
     while True:
