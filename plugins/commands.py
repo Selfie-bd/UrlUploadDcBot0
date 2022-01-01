45c02a9019d7ff5dad17225b060f8618ab560045
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-01 23:14:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,6 +13,7 @@ else:
 from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from plugins.broadcast import broadcast_handler
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
