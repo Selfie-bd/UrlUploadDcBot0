36f4effefd456e9e059451da19a9400e126240c5
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-02 17:59:49 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,13 +1,41 @@
 import datetime
 import motor.motor_asyncio
+import threading
 
-class Database:
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
 
-    def __init__(self, uri, database_name):
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+def start() -> scoped_session:
+    engine = create_engine(Config.DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
+
+INSERTION_LOCK = threading.RLock()
+
+class Database(BASE):
+    __tablename__ = "database"
+    id = Column(Integer, primary_key=True)
+    msg_id = Column(Integer)
+    
+    def __init__(self, uri, database_name, id, msg_id):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.clinton = self._client[database_name]
         self.col = self.clinton.USERS
-
+        self.id = id
+        self.msg_id = msg_id
+        
     def new_user(self, id):
         return dict(id=id, thumbnail=None)
 
@@ -37,3 +65,29 @@ class Database:
     async def get_thumbnail(self, id):
         user = await self.col.find_one({'id': int(id)})
         return user.get('thumbnail', None)
+    
+    async def df_thumb(id, msg_id):
+    with INSERTION_LOCK:
+        msg = SESSION.query(Thumbnail).get(id)
+        if not msg:
+            msg = Thumbnail(id, msg_id)
+            SESSION.add(msg)
+            SESSION.flush()
+        else:
+            SESSION.delete(msg)
+            file = Thumbnail(id, msg_id)
+            SESSION.add(file)
+        SESSION.commit()
+    
+    async def del_thumb(id):
+    with INSERTION_LOCK:
+        msg = SESSION.query(Thumbnail).get(id)
+        SESSION.delete(msg)
+        SESSION.commit()
+
+   async def thumb(id):
+        try:
+            t = SESSION.query(Thumbnail).get(id)
+            return t
+        finally:
+            SESSION.close()
