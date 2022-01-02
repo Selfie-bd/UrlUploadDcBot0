625e48023fa613750664040cd05fee5ce75ad3a5
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-02 18:49:42 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,119 +1,41 @@
-import traceback
-import datetime
-import asyncio 
-import string 
-import random 
-import time 
-import os
-import aiofiles
-import aiofiles.os
-
-from database.database.access import clinton
-
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
-
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
-broadcast_ids = {}
-
-async def send_msg(user_id, message):
-    try:
-        await message.copy(chat_id=user_id)
-        return 200, None
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return send_msg(user_id, message)
-    except InputUserDeactivated:
-        return 400, f"{user_id} : deactivated\n"
-    except UserIsBlocked:
-        return 400, f"{user_id} : blocked the bot\n"
-    except PeerIdInvalid:
-        return 400, f"{user_id} : user id invalid\n"
-    except Exception as e:
-        return 500, f"{user_id} : {traceback.format_exc()}\n"
+import datetime
+import time
+from database.users_chats_db import db
+from info import ADMINS
+from utils import broadcast_messages
+import asyncio
         
-
-@Client.on_message(filters.private & filters.command('broadcast') & filters.reply)
-async def broadcast_(c, m):
-    if m.from_user.id != Config.OWNER_ID:
-        return
-    all_users = await clinton.get_all_users()
-    
-    broadcast_msg = m.reply_to_message
-    
-    while True:
-        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    
-    out = await m.reply_text(
-        text = f"Broadcast initiated! You will be notified with log file when all the users are notified."
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+# https://t.me/GetTGLink/4178
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting your messages...'
     )
     start_time = time.time()
-    total_users = await clinton.total_users_count()
+    total_users = await db.total_users_count()
     done = 0
-    failed = 0
+    blocked = 0
+    deleted = 0
+    failed =0
+
     success = 0
-    
-    broadcast_ids[broadcast_id] = dict(
-        total = total_users,
-        current = done,
-        failed = failed,
-        success = success
-    )
-    
-    async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
-        async for user in all_users:
-            
-            sts, msg = await send_msg(
-                user_id = int(user['id']),
-                message = broadcast_msg
-            )
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            
-            if sts == 200:
-                success += 1
-            else:
+    async for user in users:
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if pti:
+            success += 1
+        elif pti == False:
+            if sh == "Bocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
                 failed += 1
-            
-            if sts == 400:
-                await clinton.delete_user(user['id'])
-            
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(
-                        current = done,
-                        failed = failed,
-                        success = success
-                    )
-                )
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
-    
-    await asyncio.sleep(3)
-    
-    await out.delete()
-    
-    if failed == 0:
-        await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    else:
-        await m.reply_document(
-            document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True
-        )
-    
-    await aiofiles.os.remove('broadcast.txt')
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}"
