5fa995c423310642962d659f2a910614545bdf72
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-11 20:27:54 +0530

    Delete plugins/database directory

--- plugins/database/adduser.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client
-from plugins.database.access import db
-from pyrogram.types import Message
-
-
-async def AddUser(bot: Client, update: Message):
-    if not await db.is_user_exist(update.from_user.id):
-           await db.add_user(update.from_user.id)
