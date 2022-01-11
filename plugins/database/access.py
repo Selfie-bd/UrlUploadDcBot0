8ed61a19503e4f1c38da072f2c02c48075f2c9a5
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-11 19:16:08 +0530

    Update access.py

--- plugins/database/access.py
+++ plugins/database/access.py
@@ -2,4 +2,4 @@
 from sample_config import Config
 from plugins.database.database import Database
 
-client = Database(Config.DB_URI, "URL-Uploader")
+db = Database(Config.DB_URI, Config.SESSION_NAME)
