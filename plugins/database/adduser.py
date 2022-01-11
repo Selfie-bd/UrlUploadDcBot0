d257a518165ccc9556d7c9bebad00e8bbc3ef8d5
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-11 19:16:39 +0530

    Update adduser.py

--- plugins/database/adduser.py
+++ plugins/database/adduser.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
-from plugins.database.access import client
+from plugins.database.access import db
 from pyrogram.types import Message
 
 
 async def AddUser(bot: Client, update: Message):
-    if not await client.is_user_exist(update.from_user.id):
-           await client.add_user(update.from_user.id)
+    if not await db.is_user_exist(update.from_user.id):
+           await db.add_user(update.from_user.id)
