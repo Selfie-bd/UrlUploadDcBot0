64c701e6fcb247b063be426773189836ed189717
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-11 15:06:50 +0530

    Update access.py

--- plugins/database/access.py
+++ plugins/database/access.py
@@ -2,4 +2,4 @@
 from sample_config import Config
 from plugins.database.database import Database
 
-clinton = Database(Config.MONGODB_URI, "URL-Uploader")
+client = Database(Config.MONGODB_URI, "URL-Uploader")
