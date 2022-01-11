e8d146624d9a734de048334a007d6b25baebe0fd
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-11 14:47:14 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,5 +1,5 @@
 import traceback, datetime, asyncio, string, random, time, os, aiofiles, aiofiles.os
-from database.access import clinton
+from plugins.database.access import clinton
 from pyrogram import filters
 from pyrogram import Client as Clinton
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
