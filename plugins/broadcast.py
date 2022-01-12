ccb99765b157a829cad84f4bbb5005a7461cc6da
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 14:15:53 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,19 +1,25 @@
-import traceback, datetime, asyncio, string, random, time, os, aiofiles, aiofiles.os
+import time
+import string
+import random
+import asyncio
+import datetime
+import aiofiles
+import traceback
+import aiofiles.os
+from sample_config import Config
 from database.access import db
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
-
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
 
 broadcast_ids = {}
 
+
 async def send_msg(user_id, message):
     try:
-        await message.copy(chat_id=user_id)
+        if Config.BROADCAST_AS_COPY is False:
+            await message.forward(chat_id=user_id)
+        elif Config.BROADCAST_AS_COPY is True:
+            await message.copy(chat_id=user_id)
         return 200, None
     except FloodWait as e:
         await asyncio.sleep(e.x)
@@ -28,82 +34,70 @@ async def send_msg(user_id, message):
         return 500, f"{user_id} : {traceback.format_exc()}\n"
 
 
-@Client.on_message(filters.private & filters.command('broadcast') & filters.reply)
-async def broadcast_(bot, update):
-    if update.from_user.id != Config.OWNER_ID:
-        return
+async def broadcast_handler(m: Message):
     all_users = await db.get_all_users()
-
-    broadcast_msg = update.reply_to_message
-
+    broadcast_msg = m.reply_to_message
     while True:
         broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
         if not broadcast_ids.get(broadcast_id):
             break
-
-    out = await update.reply_text(
-        text = f"Broadcast initiated! You will be notified with log file when all the users are notified."
+    out = await m.reply_text(
+        text=f"Broadcast Started! You will be notified with log file when all the users are notified."
     )
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
     failed = 0
     success = 0
-
     broadcast_ids[broadcast_id] = dict(
-        total = total_users,
-        current = done,
-        failed = failed,
-        success = success
+        total=total_users,
+        current=done,
+        failed=failed,
+        success=success
     )
-
     async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
         async for user in all_users:
-
             sts, msg = await send_msg(
-                user_id = int(user['id']),
-                message = broadcast_msg
+                user_id=int(user['id']),
+                message=broadcast_msg
             )
             if msg is not None:
                 await broadcast_log_file.write(msg)
-
             if sts == 200:
                 success += 1
             else:
                 failed += 1
-
             if sts == 400:
                 await db.delete_user(user['id'])
-
             done += 1
             if broadcast_ids.get(broadcast_id) is None:
                 break
             else:
                 broadcast_ids[broadcast_id].update(
                     dict(
-                        current = done,
-                        failed = failed,
-                        success = success
+                        current=done,
+                        failed=failed,
+                        success=success
                     )
                 )
     if broadcast_ids.get(broadcast_id):
         broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
-
+    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
     await asyncio.sleep(3)
-
     await out.delete()
-
     if failed == 0:
-        await update.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+        await m.reply_text(
+            text=f"broadcast completed in `{completed_in}`\n\n"
+                 f"Total users {total_users}.\n"
+                 f"Total done {done}, {success} success and {failed} failed.",
             quote=True
         )
     else:
-        await update.reply_document(
+        await m.reply_document(
             document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            caption=f"broadcast completed in `{completed_in}`\n\n"
+                    f"Total users {total_users}.\n"
+                    f"Total done {done}, {success} success and {failed} failed.",
             quote=True
         )
-
     await aiofiles.os.remove('broadcast.txt')
