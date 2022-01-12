9de1840ccf0592d2e371b447d567984788f6aec2
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 14:40:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,6 +6,7 @@ import shutil
 import string
 import asyncio
 
+
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
@@ -13,6 +14,19 @@ else:
 from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+#############################################################
+
+
+
+
+
+from client import Client
+from database.access import db
+from helper_funcs.display_progress import humanbytes
+from plugins.broadcast import broadcast_handler
+
+###############################################################
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
@@ -38,3 +52,32 @@ async def get_me_info(bot, update):
         disable_web_page_preview=True,
         reply_markup=Translation.ABOUT_BUTTONS
     )
+
+
+####################################################################################################################
+
+@Client.on_message(filters.command(["status"]) & filters.user(Config.OWNER_ID) & ~filters.edited)
+async def status_handler(_, m: Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    total_users = await db.total_users_count()
+    await m.reply_text(
+        text=f"**Total Disk Space:** {total} \n"
+             f"**Used Space:** {used}({disk_usage}%) \n"
+             f"**Free Space:** {free} \n"
+             f"**CPU Usage:** {cpu_usage}% \n"
+             f"**RAM Usage:** {ram_usage}%\n\n"
+             f"**Total Users in DB:** `{total_users}`",
+        parse_mode="Markdown",
+        quote=True
+    )
+
+
+@Client.on_message(filters.command(["broadcast"]) & filters.user(Config.OWNER_ID) & filters.reply & ~filters.edited)
+async def broadcast_in(_, m: Message):
+    await broadcast_handler(m)    
