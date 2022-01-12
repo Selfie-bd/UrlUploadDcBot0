83ee9b73ec5069b55835e1f9e9eb96425a5032af
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 15:37:22 +0530

    Delete database directory

--- database/access.py
+++
@@ -1,5 +0,0 @@
-
-from sample_config import Config
-from database.database import Database
-
-db = Database(Config.MONGODB_URI, "Url-Bot")
--- database/adduser.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client
-from database.access import db
-from pyrogram.types import Message
-
-
-async def AddUser(bot, update):
-    if not await db.is_user_exist(update.from_user.id):
-           await db.add_user(update.from_user.id)
