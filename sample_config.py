5f39a2fbdf29be9307765d7c1f538e4e97b04d4c
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 23:45:20 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,92 +1,38 @@
 import os
-import logging
-import re
-from os import environ
-
-id_pattern = re.compile(r'^.\d+$')
-
-logging.basicConfig(
-    format='%(name)s - %(levelname)s - %(message)s',
-    handlers=[logging.FileHandler('log.txt'),
-              logging.StreamHandler()],
-    level=logging.INFO
-)
 
 class Config(object):
-
+    # get a token from https://chatbase.com
+    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # The Telegram API things
-    # Get these values from my.telegram.org
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    
-    ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-    
-    #Session Name
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
-    
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    
-    #Owner ID
-    BOT_OWNER = os.environ.get("BOT_OWNER", "1307550350")
-    
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
-    
-    #OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
-
+    # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-
+    # Banned Unwanted Members..
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
     # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 2097152000
-
+    FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
-    #CHUNK_SIZE = 128
-
-    # Generate screenshots for file after uploading
-    # Defaults to True
-    SCREENSHOTS = os.environ.get("SCREENSHOTS", "True")
-
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
-
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-    
-    # Update channel for Force Subscribe
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
-    
-    # Log channel for banning spammers
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
-    
-    LOGGER = logging
-
+    # https://t.me/hevcbay/951
+    OUO_IO_API_KEY = ""
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
-
     # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = int(os.environ.get("TIME_LIMIT"))
-    
-    # dict to hold the ReQuest queue
-    ADL_BOT_RQ = {}
-
+    PROCESS_MAX_TIMEOUT = 3600
     # watermark file
     DEF_WATER_MARK_FILE = ""
-
-    # Sql Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
-    
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-    
-    # Banned Unwanted Members..
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-    
-    #Broadcast
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
