de1e14453447bdcdea871a37e128e36c950fc3c0
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 16:59:45 +0530

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -2,5 +2,5 @@ from database.access import db
 
 
 async def AddUser(bot, update):
-    if not await bd.is_user_exist(update.from_user.id):
+    if not await db.is_user_exist(update.from_user.id):
            await db.add_user(update.from_user.id)
