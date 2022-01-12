6d06e98c5beee9a1bf577739cf2e7aca07e467f1
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 22:27:56 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 import datetime
 import time
-from database.users_chats_db import db
+from plugins.users_chats_db import db
 from sample_config import Config
 import asyncio
 
