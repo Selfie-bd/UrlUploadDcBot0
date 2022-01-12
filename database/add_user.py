e892be056a4d0f8da3b5e3830ab72af9e1293520
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 12:51:12 +0530

    Create add_user.py

---
+++ database/add_user.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+from database.database import db
+from pyrogram.types import Message
+
+
+async def AddUser(bot: Client, update: Message):
+    if not await db.is_user_exist(update.from_user.id):
+           await db.add_user(update.from_user.id)
