536865cad875a4ceb467e82d89ecd3d28dc99b6a
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 12:55:54 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,8 +1,7 @@
 import traceback
 import os
 
-from pyrogram import Client
-from pyrogram import filters
+from pyrogram import Client, filters
 
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
@@ -11,8 +10,8 @@ else:
 from database.access import db
 
 @Client.on_message(filters.private & filters.command(["total"]))
-async def sts(c, m):
-    if m.from_user.id != Config.OWNER_ID:
+async def sts(bot, update):
+    if update.from_user.id != Config.OWNER_ID:
         return 
     total_users = await db.total_users_count()
-    await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
+    await update.reply_text(text=f"Total user(s) {total_users}", quote=True)
