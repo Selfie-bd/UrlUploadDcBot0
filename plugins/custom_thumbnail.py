b38e0be811e1e7173e21c607b08374823f4228b3
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-13 01:15:00 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -1,118 +1,108 @@
+import os
 import logging
+import pyrogram
+import database.database as sql
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-import random
-import numpy
-import os
-from PIL import Image
-import time
-# the secret configuration specific things
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 if bool(os.environ.get("WEBHOOK", False)):
+
     from sample_config import Config
 else:
     from config import Config
-# the Strings used for this "thing"
-from translation import Translation
 
-from pyrogram import Client
+from PIL import Image
 from pyrogram import filters
+from translation import Translation
+from database.database import *
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-from database.database import db
-from database.database import AddUser
-
-from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
-
-@Client.on_message(filters.private & filters.photo)
+@Client.on_message(filters.photo)
 async def save_photo(bot, update):
-    await AddUser(bot, update)
-    await db.set_thumbnail(update.from_user.id, thumbnail=update.photo.file_id)
-    await bot.send_message(chat_id=update.chat.id, text="**Thumbnail saved successfully**", reply_to_message_id=update.message_id)
+  
+    if update.media_group_id is not None:
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
+        if not os.path.isdir(download_location):
+            os.makedirs(download_location)
+        await sql.df_thumb(update.from_user.id, update.message_id)
+        await bot.download_media(
+              message=update,
+              file_name=download_location
+        )
+    else:
+        # received single photo
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+        await sql.df_thumb(update.from_user.id, update.message_id)
+        await bot.download_media(
+            message=update,
+            file_name=download_location
+        )
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text="**Thumbnail saved successfully**",
+            reply_to_message_id=update.message_id
+        )
 
-@Client.on_message(filters.private & filters.command(["delthumb"]))
-async def delthumbnail(bot, update):
-    await AddUser(bot, update)
-    await db.set_thumbnail(update.from_user.id, thumbnail=None)
-    await bot.send_message(chat_id=update.chat.id, text="**✅ Custom Thumbnail cleared succesfully**", reply_to_message_id=update.message_id)
 
 @Client.on_message(filters.private & filters.command(["showthumb"]))
-async def viewthumbnail(bot, update):
-    await AddUser(bot, update)
-    thumbnail = await db.get_thumbnail(update.from_user.id)
-    if thumbnail is not None:
-        await bot.send_photo(
-        chat_id=update.chat.id,
-        photo=thumbnail,
-        caption=f"Your current saved thumbnail 🦠",
-        reply_to_message_id=update.message_id)
-    else:
-        await update.reply_text(text=f"No Thumbnail found 🤒")
+async def show_thumb(bot, update):
+    if update.from_user.id in Config.BANNED_USERS:
+        await bot.delete_messages(
+              chat_id=update.chat.id,
+              message_ids=update.message_id,
+              revoke=True
+        )
+        return
 
-async def Gthumb01(bot, update):
     thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    db_thumbnail = await db.get_thumbnail(update.from_user.id)
-    if db_thumbnail is not None:
-        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
-        Image.open(thumbnail).convert("RGB").save(thumbnail)
-        img = Image.open(thumbnail)
-        img.resize((100, 100))
-        img.save(thumbnail, "JPEG")
-    else:
-        thumbnail = None
+    if not os.path.exists(thumb_image_path):
+        mes = await sthumb(update.from_user.id)
+        if mes != None:
+            m = await bot.get_messages(update.chat.id, mes.msg_id)
+            await m.download(file_name=thumb_image_path)
+            thumb_image_path = thumb_image_path
+        else:
+            thumb_image_path = None    
+    
+    if thumb_image_path is not None:
+         await bot.send_photo(
+               chat_id=update.chat.id,
+               photo=thumb_image_path,
+               caption=f"Your current saved thumbnail 🦠",
+               reply_to_message_id=update.message_id)
 
-    return thumbnail
+        
+    elif thumb_image_path is None:
+         await bot.send_message(
+               chat_id=update.chat.id,
+               text=f"No Thumbnail found 🤒",
+               reply_to_message_id=update.message_id)
 
-async def Gthumb02(bot, update, duration, download_directory):
+
+@Client.on_message(filters.private & filters.command(["delthumb"]))
+async def delete_thumbnail(bot, update):
+    if update.from_user.id in Config.BANNED_USERS:
+        await bot.delete_messages(
+              chat_id=update.chat.id,
+              message_ids=update.message_id,
+              revoke=True
+        )
+        return
+
     thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    db_thumbnail = await db.get_thumbnail(update.from_user.id)
-    if db_thumbnail is not None:
-        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
-    else:
-        thumbnail = await take_screen_shot(download_directory, os.path.dirname(download_directory), random.randint(0, duration - 1))
-
-    return thumbnail
-
-async def Mdata01(download_directory):
-
-          width = 0
-          height = 0
-          duration = 0
-          metadata = extractMetadata(createParser(download_directory))
-          if metadata is not None:
-              if metadata.has("duration"):
-                  duration = metadata.get('duration').seconds
-              if metadata.has("width"):
-                  width = metadata.get("width")
-              if metadata.has("height"):
-                  height = metadata.get("height")
-
-          return width, height, duration
-
-async def Mdata02(download_directory):
-
-          width = 0
-          duration = 0
-          metadata = extractMetadata(createParser(download_directory))
-          if metadata is not None:
-              if metadata.has("duration"):
-                  duration = metadata.get('duration').seconds
-              if metadata.has("width"):
-                  width = metadata.get("width")
-
-          return width, duration
-
-async def Mdata03(download_directory):
-
-          duration = 0
-          metadata = extractMetadata(createParser(download_directory))
-          if metadata is not None:
-              if metadata.has("duration"):
-                  duration = metadata.get('duration').seconds
-
-          return duration        
+    
+    try:
+        await sql.dthumb(update.from_user.id)
+        os.remove(thumb_image_path)
+    except:
+        pass
+    await bot.send_message(
+          chat_id=update.chat.id,
+          text="**✅ Custom Thumbnail cleared succesfully**",
+          reply_to_message_id=update.message_id)        
