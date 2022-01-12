423c6987d26ff87af5263e2a7a0225019f4ee136
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 13:37:45 +0530

    Update access.py

--- database/access.py
+++ database/access.py
@@ -2,4 +2,4 @@
 from sample_config import Config
 from database.database import Database
 
-clinton = Database(Config.DATABASE_URL, Config.SESSION_NAME)
+db = Database(Config.DATABASE_URL, Config.SESSION_NAME)
