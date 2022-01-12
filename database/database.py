a92e7c96226859ca93bb9e27b5d52f39a1e358f6
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 21:14:45 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,9 +1,12 @@
 import os
+
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
+
 import threading
+
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,13 +14,17 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 
 def start() -> scoped_session:
-    engine = create_engine(Config.MONGODB_URI, client_encoding="utf8")
+    engine = create_engine(Config.DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
 BASE = declarative_base()
 SESSION = start()
+
 INSERTION_LOCK = threading.RLock()
+
 class Thumbnail(BASE):
     __tablename__ = "thumbnail"
     id = Column(Integer, primary_key=True)
@@ -26,7 +33,9 @@ class Thumbnail(BASE):
     def __init__(self, id, msg_id):
         self.id = id
         self.msg_id = msg_id
+
 Thumbnail.__table__.create(checkfirst=True)
+
 async def df_thumb(id, msg_id):
     with INSERTION_LOCK:
         msg = SESSION.query(Thumbnail).get(id)
@@ -39,11 +48,13 @@ async def df_thumb(id, msg_id):
             file = Thumbnail(id, msg_id)
             SESSION.add(file)
         SESSION.commit()
+
 async def del_thumb(id):
     with INSERTION_LOCK:
         msg = SESSION.query(Thumbnail).get(id)
         SESSION.delete(msg)
         SESSION.commit()
+
 async def thumb(id):
     try:
         t = SESSION.query(Thumbnail).get(id)
