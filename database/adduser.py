896d3eba8bd0e600806586f315a7faad94fcf025
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 15:27:43 +0530

    Update and rename add.py to adduser.py

---
+++ database/adduser.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+from database.access import db
+from pyrogram.types import Message
+
+
+async def AddUser(bot, update):
+    if not await db.is_user_exist(update.from_user.id):
+           await db.add_user(update.from_user.id)
