e31020c8b31e0c76cfee844d09abbf75fe0814f6
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 20:02:16 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -20,7 +20,7 @@ else:
 
 
 def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI, client_encoding="utf8")
+    engine = create_engine(Config.MONGODB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
