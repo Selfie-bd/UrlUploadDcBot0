760312ab107aa1018ad45bb03acdb1a2e4f6a448
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 15:31:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,28 +56,8 @@ async def get_me_info(bot, update):
 
 ####################################################################################################################
 
-@Client.on_message(filters.command(["status"]) & filters.user(Config.OWNER_ID) & ~filters.edited)
-async def status_handler(_, m: Message):
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
-    await m.reply_text(
-        text=f"**Total Disk Space:** {total} \n"
-             f"**Used Space:** {used}({disk_usage}%) \n"
-             f"**Free Space:** {free} \n"
-             f"**CPU Usage:** {cpu_usage}% \n"
-             f"**RAM Usage:** {ram_usage}%\n\n"
-             f"**Total Users in DB:** `{total_users}`",
-        parse_mode="Markdown",
-        quote=True
-    )
 
 
 @Client.on_message(filters.command(["broadcast"]) & filters.user(Config.OWNER_ID) & filters.reply & ~filters.edited)
-async def broadcast_in(_, m: Message):
-    await broadcast_handler(m)    
+async def broadcast_in(bot, update):
+    await broadcast_handler(update)    
