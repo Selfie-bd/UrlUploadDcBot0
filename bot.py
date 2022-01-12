a69d1de919148fe61d95f7fbceb4786ff2f4f3ed
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 23:44:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,15 @@ logger = logging.getLogger(__name__)
 
 import os
 
+# the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
 
-from pyrogram import Client
-from pyrogram import filters
+import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 if __name__ == "__main__" :
     # create download directory, if not exist
@@ -21,10 +22,12 @@ if __name__ == "__main__" :
     plugins = dict(
         root="plugins"
     )
-    app = Client(
-        "URL Uploader",
-        bot_token=Config.BOT_TOKEN,
+    app = pyrogram.Client(
+        "AnyDLBot",
+        bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins)
-    app.run() 
+        plugins=plugins
+    )
+    Config.AUTH_USERS
+    app.run()
