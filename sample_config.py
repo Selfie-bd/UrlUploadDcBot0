3a8073e88b623fed1546bc48f64404b737d66253
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 16:53:56 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -91,7 +91,7 @@ class Config(object):
     # Sql Database url
     ##DB_URI = os.environ.get("DATABASE_URL", "")
     #Session Name
-    ##SESSION_NAME = os.environ.get("SESSION_NAME", "")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "")
     ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
     DATABASE_URI = environ.get('DATABASE_URI', "")
     ##DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
