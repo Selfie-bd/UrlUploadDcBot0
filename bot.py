89b10d1f9c4c793ccd4cb9b4f3c191b5d5862a7d
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-12 21:01:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,15 @@ logger = logging.getLogger(__name__)
 
 import os
 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
 
 from pyrogram import Client
-from pyrogram import filters
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 if __name__ == "__main__" :
     # create download directory, if not exist
@@ -23,8 +24,10 @@ if __name__ == "__main__" :
     )
     app = Client(
         "URL Uploader",
-        bot_token=Config.BOT_TOKEN,
+        bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins)
-    app.run() 
+        plugins=plugins
+    )
+    Config.AUTH_USERS
+    app.run()
