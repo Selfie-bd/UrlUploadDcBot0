81ea0c251d35cecfa4a7cf29374c4eaec727419b
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-13 11:24:09 +0530

    Update cd_buttons.py

--- plugins/cd_buttons.py
+++ plugins/cd_buttons.py
@@ -1,129 +1,23 @@
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
-import json
-import math
-import os
-import shutil
-import subprocess
-import time
 
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
+
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
 from translation import Translation
 from pyrogram import Client
-from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
-from plugins.dl_button import ddl_call_back
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-from PIL import Image
-
 
 
 @Client.on_callback_query()
 async def button(bot, update):
-    if update.from_user.id in Config.BANNED_USERS:
-        await bot.delete_messages(
-            chat_id=update.message.chat.id,
-            message_ids=update.message.message_id,
-            revoke=True
-        )
-        return
-    # logger.info(update)
-    cb_data = update.data
-    if ":" in cb_data:
-        # unzip formats
-        extract_dir_path = Config.DOWNLOAD_LOCATION + \
-            "/" + str(update.from_user.id) + "zipped" + "/"
-        if not os.path.isdir(extract_dir_path):
-            await bot.delete_messages(
-                chat_id=update.message.chat.id,
-                message_ids=update.message.message_id,
-                revoke=True
-            )
-            return False
-        zip_file_contents = os.listdir(extract_dir_path)
-        type_of_extract, index_extractor, undefined_tcartxe = cb_data.split(":")
-        if index_extractor == "NONE":
-            try:
-                shutil.rmtree(extract_dir_path)
-            except:
-                pass
-            await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                text=Translation.CANCEL_STR,
-                message_id=update.message.message_id
-            )
-        elif index_extractor == "ALL":
-            i = 0
-            for file_content in zip_file_contents:
-                current_file_name = os.path.join(extract_dir_path, file_content)
-                start_time = time.time()
-                await bot.send_document(
-                    chat_id=update.message.chat.id,
-                    document=current_file_name,
-                    # thumb=thumb_image_path,
-                    caption=file_content,
-                    # reply_markup=reply_markup,
-                    reply_to_message_id=update.message.message_id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
-                    )
-                )
-                i = i + 1
-                os.remove(current_file_name)
-            try:
-                shutil.rmtree(extract_dir_path)
-            except:
-                pass
-            await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                text=Translation.ZIP_UPLOADED_STR.format(i, "0"),
-                message_id=update.message.message_id
-            )
-        else:
-            file_content = zip_file_contents[int(index_extractor)]
-            current_file_name = os.path.join(extract_dir_path, file_content)
-            start_time = time.time()
-            await bot.send_document(
-                chat_id=update.message.chat.id,
-                document=current_file_name,
-                # thumb=thumb_image_path,
-                caption=file_content,
-                # reply_markup=reply_markup,
-                reply_to_message_id=update.message.message_id,
-                progress=progress_for_pyrogram,
-                progress_args=(
-                    Translation.UPLOAD_START,
-                    update.message,
-                    start_time
-                )
-            )
-            try:
-                shutil.rmtree(extract_dir_path)
-            except:
-                pass
-            await bot.edit_message_text(
-                chat_id=update.message.chat.id,
-                text=Translation.ZIP_UPLOADED_STR.format("1", "0"),
-                message_id=update.message.message_id
-            )
-    elif "|" in update.data:
+    if "|" in update.data:
         await youtube_dl_call_back(bot, update)
     elif "=" in update.data:
         await ddl_call_back(bot, update)
-
     elif update.data == "home":
         await update.message.edit_text(
             text=Translation.START_TEXT.format(update.from_user.mention),
@@ -148,5 +42,5 @@ async def button(bot, update):
             reply_markup=Translation.DONATE_BUTTONS,
             disable_web_page_preview=True
         )
-    elif "closeformat" in update.data:     
+    else:
         await update.message.delete()
