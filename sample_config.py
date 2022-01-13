f65c5f094e96c6165c16e3d455d4a354832a378d
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2022-01-13 12:25:08 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,36 +1,14 @@
 import os
-import re
-from os import environ
-import logging
-logger = logging.getLogger(__name__)
-
-id_pattern = re.compile(r'^.\d+$')
-
-logging.basicConfig(
-    format='%(name)s - %(levelname)s - %(message)s',
-    handlers=[logging.FileHandler('log.txt'),
-              logging.StreamHandler()],
-    level=logging.INFO
-)
 
 class Config(object):
 
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     # The Telegram API things
     # Get these values from my.telegram.org
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    
-    
-    
-    #Owner ID
-    BOT_OWNER = os.environ.get("BOT_OWNER", "1307550350")
-    
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
-    
-    #OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
 
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
@@ -57,11 +35,6 @@ class Config(object):
     
     # Update channel for Force Subscribe
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
-    
-    # Log channel for banning spammers
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
-    
-    LOGGER = logging
 
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
@@ -75,28 +48,8 @@ class Config(object):
     # watermark file
     DEF_WATER_MARK_FILE = ""
 
-    
-    
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-    
-    # Banned Unwanted Members..
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-    
-    #Broadcast
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-    # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
     # Sql Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
-    #Session Name
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
-    ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-    DATABASE_URI = environ.get('DATABASE_URI', "")
-    DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
-    try:
-        TRACE_CHANNEL = int(os.environ.get("TRACE_CHANNEL")) if os.environ.get("TRACE_CHANNEL", "") else None
-    except:
-        TRACE_CHANNEL = None
-        logger.warning("Trace channel id was invalid")
+    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    
+    # Database Name
+    DATABASE_NAME = environ.get('DATABASE_NAME', "URL-BOT")
